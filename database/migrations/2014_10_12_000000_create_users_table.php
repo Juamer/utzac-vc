6a71a99d9b6ab8cb15eb9d71d92a8053a14c84bb
Juan Pablo <juanpablo3899@gmail.com>
2019-03-28 10:13:18 -0600

    Tabla usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('apellidos');
+            $table->string('nom_usu');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('matricula')->unique();
+            $table->string('solicitud');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
